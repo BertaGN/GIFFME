d8c0f84c4cff74fe1cab7db87e3080ccb0443c26
Berta Garcia <bertagnavarro93@gmail.com>
2023-05-24 18:12:32 +0200

    styled app

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BiSearchAlt } from 'react-icons/bi';
 
 const Search = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -14,21 +15,21 @@ const Search = ({ onSearch }) => {
 
   return (
     <form onSubmit={handleSubmit} className="flex items-center">
-    <input
-      type="text"
-      value={searchTerm}
-      onChange={handleInputChange}
-      placeholder="Type your search"
-      className="border border-gray-300 rounded-l px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-    />
-    <button
-      type="submit"
-      className="text-white font-bold py-2 px-4 rounded-r bg-gradient-to-r from-pink-500 to-pink-600 hover:from-pink-600 hover:to-pink-700"
-    >
-      Search
-    </button>
-  </form>
-);
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
+        placeholder="Type your search"
+        className="border border-gray-300 rounded-l px-4 py-2 h-8 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <button
+        type="submit"
+        className="text-white font-bold py-2 px-4 rounded-r bg-gradient-to-r from-pink-500 to-pink-600 hover:from-pink-600 hover:to-pink-700"
+      >
+        <BiSearchAlt size={16} />
+      </button>
+    </form>
+  );
 };
 
 export default Search;
